c84151f9a1cedd134ebbd7eef9e72b1ada165958
Jack Ojango <ojangoh2@outlook.com>
2023-09-22 11:28:39 +0300

    Authentication

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,5 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// firebase.js
+
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, push } from 'firebase/database';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,5 +17,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export the Firebase instance if needed
-export const firebaseInstance = app;
+// Get a reference to the database service
+const database = getDatabase();
+
+export { database, ref, push };
--- firebase/signup.js
+++ firebase/signup.js
@@ -1,4 +1,29 @@
-//create sign up with eamil and password and export it to the sign up page  
-export const signUpWithEmailPassword = (email, password) => {
-    return firebase.auth().createUserWithEmailAndPassword(email, password);
+// signup.js
+
+import { database, ref, push } from './firebase';
+
+const signUpWithEmailPassword = (email, password, firstName, lastName) => {
+  return new Promise((resolve, reject) => {
+    // Create a new user with email and password
+    auth.createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        const timestamp = Date.now();
+        const userRef = push(ref(database, 'users'), {
+          email: user.email,
+          firstName: firstName,
+          lastName: lastName,
+          timestamp: timestamp
+        });
+        resolve(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        reject(errorMessage);
+      });
+  });
 };
+
+export { signUpWithEmailPassword };
