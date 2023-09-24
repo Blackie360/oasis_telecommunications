b55dbc2424d6c5dce5475951dda407972648dfc2
Jack Ojango <ojangoh2@outlook.com>
2023-09-24 10:19:01 +0300

    main.js

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ import './src/styles/header.css';
 import './src/styles/footer.css';
 import { createHeader } from './src/components/header';
 import { createFooter } from './src/components/footer';
-//import { createHomePage } from './src/components/homepage';
-// Import our custom CSS
 import './src/styles/scss/styles.scss';
 
 // Import Bootstrap's JS (assuming you're using ES6 modules)
@@ -12,13 +10,10 @@ import * as bootstrap from 'bootstrap';
 
 const footerContent = document.getElementById('footer');
 const headerContent = document.getElementById('header');
-//const homepageContent = document.getElementById('homepage');
+
 
 // Create and insert the header component
 createHeader(headerContent);
-
-// Create and insert the main component
-//createHomePage(homepageContent);
 
 // Create and insert the footer component
 createFooter(footerContent);
